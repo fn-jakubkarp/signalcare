b06a1d211addcc4f9798769afda8a5cd4a10178a
quni <quni.inne@gmail.com>
2024-07-16 23:29:49 +0200

    fix: createUser returning undefined

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -14,38 +14,43 @@ import {
 } from "../appwrite.config";
 import { parseStringify } from "../utils";
 
+
 export const createUser = async (user: CreateUserParams) => {
 	try {
-		const newUser = await users.create(
+		const newuser = await users.create(
 			ID.unique(),
 			user.email,
 			user.phone,
 			undefined,
 			user.name
 		);
+
+		return parseStringify(newuser);
 	} catch (error: any) {
+		// Check existing user
 		if (error && error?.code === 409) {
 			const documents = await users.list([
 				Query.equal("email", [user.email])
 			]);
 
-			return documents?.users[0];
+			return documents.users[0];
 		}
+		console.error("An error occurred while creating a new user:", error);
 	}
 };
 
 export const getUser = async (userId: string) => {
 	try {
-	  const user = await users.get(userId);
-  
-	  return parseStringify(user);
+		const user = await users.get(userId);
+
+		return parseStringify(user);
 	} catch (error) {
-	  console.error(
-		"An error occurred while retrieving the user details:",
-		error
-	  );
+		console.error(
+			"An error occurred while retrieving the user details:",
+			error
+		);
 	}
-  };
+};
 
 export const registerPatient = async ({
 	identificationDocument,
