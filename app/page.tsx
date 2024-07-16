cbe9e16a1e4d73e3a3f76add3ed64c4c5ebb4295
quni <quni.inne@gmail.com>
2024-07-16 23:20:31 +0200

    chore: comment out unused

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
 import Link from "next/link";
+import PatientForm from "@/components/forms/PatientForm";
 
-// import { PatientForm } from "@/components/forms/PatientForm";
 // import { PasskeyModal } from "@/components/PasskeyModal";
 
 const Home = ({ searchParams }: SearchParamProps) => {
-	const isAdmin = searchParams?.admin === "true";
+	// const isAdmin = searchParams?.admin === "true";
 
 	return (
 		<div className="flex h-screen max-h-screen">
